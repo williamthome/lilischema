9d9a6c2cfa2a1d755f2b9445631e3d26374e043a
William Fank Thomé <willegp88@gmail.com>
2021-04-25 14:28:55 -0300

    refactor: use entry point only for exports

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
-import type { ExtractSchema } from './schemas/protocols'
-import { StringValidator } from './validators/models'
-import { Schema } from './schemas'
-
-const requiredStringValidator = new StringValidator('required')
-const valid = requiredStringValidator.validate('Valid value')
-const invalid = requiredStringValidator.validate(123)
-console.log('IsStringValidation result:', { valid, invalid })
-
-const fooSchema = new Schema(
-  {
-    foo: requiredStringValidator,
-    bar: new Schema(
-      {
-        foo: requiredStringValidator,
-      },
-      'optional',
-    ),
-  },
-  'required',
-)
-
-type FooSchemaType = ExtractSchema<typeof fooSchema>
-const foo: FooSchemaType = {
-  foo: 'bar',
-  bar: {
-    foo: 'bar',
-  },
-}
-const validSchema = fooSchema.validate(foo)
-const invalidSchema = fooSchema.validate()
-console.log('SchemaValidation result:', { validSchema, invalidSchema })
+export * from '@/schemas/protocols'
+export * from '@/schemas'
+export * from '@/validations/protocols'
+export * from '@/validations/models'
+export * from '@/validations'
+export * from '@/validators/models'
+export * from '@/validators'
