eb1712f5f9775472613724c378f21b10aa877b3a
William Fank Thomé <willegp88@gmail.com>
2021-04-26 15:04:07 -0300

    refactor: move validations interfaces to a folder

--- src/validations/protocols/interfaces/validation.ts
+++ src/validations/protocols/interfaces/validation.ts
@@ -1,5 +1,9 @@
 import type { ITypeWrapper } from '@/common/protocols'
-import type { ValidateFunction, ValidatePayload, ValidationType } from './types'
+import type {
+  ValidateFunction,
+  ValidatePayload,
+  ValidationType,
+} from '../types'
 
 export interface IDoValidation<T> extends ITypeWrapper<T> {
   validate: ValidateFunction
